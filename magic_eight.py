9e35f108b10824bb33acd9b0153fd85010c56b2e
Alex <careyalc@umich.edu>
2018-01-29 20:32:02 -0500

    it works

--- magic_eight.py
+++ magic_eight.py
@@ -1,3 +1,5 @@
+import random
+
 def get_question(question=input("What is your question?")):
 	while question != "quit":
 		if question[-1] == "?":
@@ -5,6 +7,11 @@ def get_question(question=input("What is your question?")):
 		else:
 			print("Sorry, I can only answer questions")
 
-list_of_possible_answers=["it is certain", "it is decidedly so", "without a doubt", "yes definitely", "you may rely on it", "as I see it, yes", "most likely", "outlook good", "yes", "signs point to yes", "reply hazy try again", "ask again later", "better not tell you now", "cannot predict now", "concentrate and ask again", "don't count on it", "my reply is no", "my sources say no", "outlook not so good", "very doubtful"]
-x = random.choice(list_of_possible_answers)
 
+
+def return_answer(q):
+	list_of_possible_answers=["it is certain", "it is decidedly so", "without a doubt", "yes definitely", "you may rely on it", "as I see it, yes", "most likely", "outlook good", "yes", "signs point to yes", "reply hazy try again", "ask again later", "better not tell you now", "cannot predict now", "concentrate and ask again", "don't count on it", "my reply is no", "my sources say no", "outlook not so good", "very doubtful"]
+	print(random.choice(list_of_possible_answers))
+
+
+return_answer(get_question())
