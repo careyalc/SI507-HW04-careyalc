0c508880852fd24d1721655769672041f71669ab
Alex <careyalc@umich.edu>
2018-01-29 20:21:58 -0500

    fixed error

--- magic_eight.py
+++ magic_eight.py
@@ -1,6 +1,6 @@
 def get_question(question=input("What is your question?")):
 	while question != "quit":
-		if question[-1] == “?”:
+		if question[-1] == "?":
 			return question
 		else:
 			print("Sorry, I can only answer questions")
